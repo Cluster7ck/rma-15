01918cc58bc6488023a4b1b825dbd700f58bf5e7
Lennart <Cluster7ck@users.noreply.github.com>
2015-06-29 13:07:49 +0200

    submit join

---
+++ app/src/js/buttons.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$("#submitJoin").submit(function(event) {
+    //vor get
+    event.preventDefault();
+//    if($(this).find('input[name="gameid"]').val() === "" || $(this).find('input[name="password"]').val()=== ""){
+//        //append Fehler box
+//    }
+//    //an Host
+//    $.get("localhost:8080",
+//            {
+//                gameid: $(this).find('input[name="gameid"]').val() ,
+//                password: $(this).find('input[name="password"]').val()
+//            })
+//    .done(function(data){
+//        //RETURN DATA 
+//    });
+//    
+
+});
